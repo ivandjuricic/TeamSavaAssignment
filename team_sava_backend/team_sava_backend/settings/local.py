fd1ac5e5875c338c0d199ab76132ba3815fa593f
Ivan Djuricic <ivandjuricic@Ivans-MacBook-Pro-2.local>
2020-04-20 11:25:42 +0200

    Adding loggers to django errors and async celery tasks

--- team_sava_backend/team_sava_backend/settings/local.py
+++ team_sava_backend/team_sava_backend/settings/local.py
@@ -1,8 +1,53 @@
+import os
 from .base import *
+
 
 """
 Place here environment dedicated settings that is not secret
 """
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        # Send all messages to console
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'celery_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'team_sava_backend/logs/views.log',
+            'maxBytes': 1024 * 1024 * 10,  # 10MB
+            'backupCount': 10,
+        },
+        'errors_file': {
+            'level': 'WARNING',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'team_sava_backend/logs/errors.log',
+            'maxBytes': 1024 * 1024 * 10,  # 10MB
+            'backupCount': 10
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console', 'errors_file'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'celery.logger': {
+            'handlers': ['celery_handler'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
 DOMAIN = "http://localhost:3000"
 
